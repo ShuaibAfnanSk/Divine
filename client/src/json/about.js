b8741a9af8a8d0633f703f157a63bcf599757a85
ShuaibAfnanSk <ivoxatomig@gmail.com>
2023-11-14 13:47:08 +0530

    added testimonials

---
+++ client/src/json/about.js
@@ -0,0 +1,25 @@
+const about = [
+    {
+        image: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "James Anderson",
+        role: "Homebuyer",
+        desc: "Thrilled with Alchemy Divine's KNN search. Found my dream home fast. Linear regression ensured a fair deal. K-means clustering made neighborhood choices a breeze. Highly recommend!",
+        rating: 5.0
+    },
+    {
+        image: "https://images.unsplash.com/photo-1552699611-e2c208d5d9cf?q=80&w=2008&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Sarah Johnson",
+        role: "Property Seller",
+        desc: "A satisfied seller on Alchemy Divine. The KNN feature connected my property with the perfect buyers. Thanks to the linear regression tool, I sold at the ideal price",
+        rating: 4.0
+    },
+    {
+        image: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "John Smith",
+        role: "Real Estate Investor",
+        desc: "As an investor on Divine. KNN identifies growth areas. Linear regression secures smart investments. K-means clustering streamlines property selection. A must for strategic real estate ventures!",
+        rating: 4.0
+    }
+]
+
+export default about;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,10 +4,18 @@ import Header from "../components/Header";
 import polaroids from "../json/polaroids";
 import service from "../json/service";
 import { useState } from "react";
+import about from "../json/about";
+import styled from "styled-components";
+
+const Slider = styled.div`
+    transform: translatex(${props => props.slider * -100}%);
+    transition: .5s ease-in-out;
+`
 
 const Home = () => {
 
     const [email, setEmail] = useState();
+    const [slider, setSlider] = useState(0);
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -18,6 +26,14 @@ const Home = () => {
                 window.location.reload()
             })
             .catch(err => console.log(err))
+    }
+
+    const handleClick = (direction) => {
+        if (direction === "left") {
+            setSlider(slider > 0 ? slider - 1 : 0)
+        } else {
+            setSlider(slider < 2 ? slider + 1 : 2)
+        }
     }
 
     return (
@@ -42,11 +58,36 @@ const Home = () => {
                 </div>
             </div>
 
-
             <div className="about">
-                <div className="aboutBx">
-                    <h3>Who we are</h3>
-                    <p>Experience seamless real estate transactions on our user-friendly platform. Discover your dream home or sell effortlessly with our simplified process. We connect buyers and sellers, ensuring a smooth and rewarding property experience. Trust us to make your real estate journey easy and enjoyable. Your dream home or successful sale is just a click away!</p>
+                <Slider className="aboutSlider" slider={slider}>
+                    {about.map((a) => (
+                        <div className="aboutDiv">
+                            <div className="aboutCard">
+                                <div className="overlay">
+                                    <img src={a.image} alt="" />
+                                </div>
+                                <div className="cardTxt">
+                                    <div className="cardDesc">
+                                        <p>{a.desc}</p>
+                                    </div>
+                                    <div className="cardStamps">
+                                        <div className="stampLeft">
+                                            <h5>{a.name}</h5>
+                                            <h6>{a.role}</h6>
+                                        </div>
+                                        <div className="stampRight">
+                                            <h3>{a.rating}.0</h3>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000" viewBox="0 0 256 256"><path d="M239.2,97.29a16,16,0,0,0-13.81-11L166,81.17,142.72,25.81h0a15.95,15.95,0,0,0-29.44,0L90.07,81.17,30.61,86.32a16,16,0,0,0-9.11,28.06L66.61,153.8,53.09,212.34a16,16,0,0,0,23.84,17.34l51-31,51.11,31a16,16,0,0,0,23.84-17.34l-13.51-58.6,45.1-39.36A16,16,0,0,0,239.2,97.29Zm-15.22,5-45.1,39.36a16,16,0,0,0-5.08,15.71L187.35,216v0l-51.07-31a15.9,15.9,0,0,0-16.54,0l-51,31h0L82.2,157.4a16,16,0,0,0-5.08-15.71L32,102.35a.37.37,0,0,1,0-.09l59.44-5.14a16,16,0,0,0,13.35-9.75L128,32.08l23.2,55.29a16,16,0,0,0,13.35,9.75L224,102.26S224,102.32,224,102.33Z"></path></svg>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </Slider>
+                <div className="aboutBtns">
+                    <button onClick={() => handleClick("left")}><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#ffffff" viewBox="0 0 256 256"><path d="M224,128a8,8,0,0,1-8,8H59.31l58.35,58.34a8,8,0,0,1-11.32,11.32l-72-72a8,8,0,0,1,0-11.32l72-72a8,8,0,0,1,11.32,11.32L59.31,120H216A8,8,0,0,1,224,128Z"></path></svg></button>
+                    <button onClick={() => handleClick("right")}><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#ffffff" viewBox="0 0 256 256"><path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path></svg></button>
                 </div>
             </div>
 
