fc409a9478da88ada127ac42502cf2c887527dc9
ShuaibAfnanSk <ivoxatomig@gmail.com>
2023-11-17 20:51:43 +0530

    added loader

--- client/src/components/Grove.jsx
+++ client/src/components/Grove.jsx
@@ -23,11 +23,18 @@ const Grove = () => {
 
     const [grove, setGrove] = useState([]);
     const [slider, setSlider] = useState(0);
+    const [loading, setLoading] = useState(true);
     axios.defaults.withCredentials = true;
     useEffect(() => {
         axios.get("https://divine-pi.vercel.app/server/properties/grove")
-            .then(result => setGrove(result.data))
-            .catch(err => console.log(err))
+            .then(result => {
+                setGrove(result.data)
+                setLoading(false)
+            })
+            .catch(err => {
+                console.log(err)
+                setLoading(false)
+            })
     }, [])
 
     const handleClick = (direction) => {
@@ -41,30 +48,39 @@ const Grove = () => {
     };
 
     return (
-        <div className="grove border">
-            <div className="title">
-                <h6>Cluster One</h6>
-                <h3>Dive-In Oasis Retreat</h3>
-            </div>
-            <div className="sliderWrapper">
-                <Slider className="slider" slider={slider}>
-                    {grove.map((p) => (
-                        <div className="slide">
-                            <Image className="slideBx" img={p.image}>
-                                <div className="slideStamps">
-                                    <Link className="link" to={`/property/${p.name}`}><h5>{p.name}</h5></Link>
-                                    <h5>${p.price}</h5>
-                                </div>
-                            </Image>
-                        </div>
-                    ))}
-                </Slider>
-                <div className="btns">
-                    <div className="btn" onClick={() => handleClick("left")}><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256"><path d="M232,200a8,8,0,0,1-16,0,88.1,88.1,0,0,0-88-88H51.31l34.35,34.34a8,8,0,0,1-11.32,11.32l-48-48a8,8,0,0,1,0-11.32l48-48A8,8,0,0,1,85.66,61.66L51.31,96H128A104.11,104.11,0,0,1,232,200Z"></path></svg></div>
-                    <div className="btn" onClick={() => handleClick("right")}><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256"><path d="M229.66,109.66l-48,48a8,8,0,0,1-11.32-11.32L204.69,112H128a88.1,88.1,0,0,0-88,88,8,8,0,0,1-16,0A104.11,104.11,0,0,1,128,96h76.69L170.34,61.66a8,8,0,0,1,11.32-11.32l48,48A8,8,0,0,1,229.66,109.66Z"></path></svg></div>
+        <>
+            {loading ? (
+                <div className="loaderWrapper">
+                    <div class="loader"></div>
                 </div>
-            </div>
-        </div>
+            ) : (
+                <div className="grove border">
+                    <div className="title">
+                        <h6>Cluster One</h6>
+                        <h3>Dive-In Oasis Retreat</h3>
+                    </div>
+                    <div className="sliderWrapper">
+                        <Slider className="slider" slider={slider}>
+                            {grove.map((p) => (
+                                <div className="slide">
+                                    <Image className="slideBx" img={p.image}>
+                                        <div className="slideStamps">
+                                            <Link className="link" to={`/property/${p.name}`}><h5>{p.name}</h5></Link>
+                                            <h5>${p.price}</h5>
+                                        </div>
+                                    </Image>
+                                </div>
+                            ))}
+                        </Slider>
+                        <div className="btns">
+                            <div className="btn" onClick={() => handleClick("left")}><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256"><path d="M232,200a8,8,0,0,1-16,0,88.1,88.1,0,0,0-88-88H51.31l34.35,34.34a8,8,0,0,1-11.32,11.32l-48-48a8,8,0,0,1,0-11.32l48-48A8,8,0,0,1,85.66,61.66L51.31,96H128A104.11,104.11,0,0,1,232,200Z"></path></svg></div>
+                            <div className="btn" onClick={() => handleClick("right")}><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256"><path d="M229.66,109.66l-48,48a8,8,0,0,1-11.32-11.32L204.69,112H128a88.1,88.1,0,0,0-88,88,8,8,0,0,1-16,0A104.11,104.11,0,0,1,128,96h76.69L170.34,61.66a8,8,0,0,1,11.32-11.32l48,48A8,8,0,0,1,229.66,109.66Z"></path></svg></div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+        </>
     );
 }
 
