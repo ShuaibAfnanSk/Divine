aa2b4fa3021e60637931fba27031fcc2c44d09a6
ShuaibAfnanSk <ivoxatomig@gmail.com>
2023-11-23 16:17:34 +0530

    changed visit page

--- client/src/pages/Visit.jsx
+++ client/src/pages/Visit.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { useContext, useEffect, useState} from "react";
+import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
-import Header from "../components/Header";
 import { Context } from "../context/Context";
+import { Link } from "react-router-dom";
 
 const VisitImg = styled.div`
     background-image: url(${props => props.img});
@@ -14,7 +14,7 @@ const VisitImg = styled.div`
 const Visit = () => {
 
     const path = location.pathname.split('/')[2];
-    const {user} = useContext(Context);
+    const { user } = useContext(Context);
     const [fullName, setFullName] = useState();
     const [name, setName] = useState();
     const [city, setCity] = useState();
@@ -24,7 +24,7 @@ const Visit = () => {
     const [date, setDate] = useState();
     const [image, setImage] = useState();
     axios.defaults.withCredentials = true;
-    
+
     useEffect(() => {
         axios.get("https://divine-pi.vercel.app/server/properties/" + path)
             .then((result) => {
@@ -47,28 +47,53 @@ const Visit = () => {
             .catch(err => console.log(err))
     }
 
+    const handleShow = () => {
+        setShow(!show)
+    }
+
+    const handleUnShow = () => {
+        setShow(!show)
+    }
+
     return (
-        <VisitImg className="visit" img={image}>
-            <Header />
-            <div className="contactBx">
-                <div className="contactLeft">
-                    <h3>{name}</h3>
-                    <p>Enter all the required details & schedule your visit as per your flexibility.</p>
-                </div>
-                <div className="contactRight">
-                    <form onSubmit={handleSubmit}>
+        <VisitImg className="contact" img={image}>
+            <div className="sellerForm contactForm">
+                <form onSubmit={handleSubmit}>
+                    <Link to={'/'} className="link linkSvg"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" viewBox="0 0 256 256"><path d="M224,128a8,8,0,0,1-8,8H59.31l58.35,58.34a8,8,0,0,1-11.32,11.32l-72-72a8,8,0,0,1,0-11.32l72-72a8,8,0,0,1,11.32,11.32L59.31,120H216A8,8,0,0,1,224,128Z"></path></svg>Back to Home</Link>
+                    <div className="sellTitle">
+                        <div className="sellTxt">
+                            <h3>Visit</h3>
+                            <p>Plan your visit.</p>
+                        </div>
+                        <div className="help" onMouseEnter={handleShow} onMouseLeave={handleUnShow}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256"><path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm16-40a8,8,0,0,1-8,8,16,16,0,0,1-16-16V128a8,8,0,0,1,0-16,16,16,0,0,1,16,16v40A8,8,0,0,1,144,176ZM112,84a12,12,0,1,1,12,12A12,12,0,0,1,112,84Z"></path></svg>
+                        </div>
+                        {show ? (<div className="helpBx">
+                            <h6>Have a Query?, Contact us through this form and our support staff will help you with it.</h6>
+                        </div>) : null}
+
+                    </div>
+                    <div className="single">
                         <label htmlFor="name">Full Name</label>
                         <input type="text" name="fullName" placeholder={user ? user.username : "john doe"} onChange={(e) => setFullName(e.target.value)} />
+                    </div>
+                    <div className="single">
+
                         <label htmlFor="email">Email</label>
                         <input type="email" name="email" id="" placeholder="example@email.com" onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className="single">
                         <label htmlFor="email">Phone</label>
                         <input type="text" name="phone" id="" placeholder="+49 176 123 456 79" onChange={(e) => setPhone(e.target.value)} />
+                    </div>
+                    <div>
                         <label htmlFor="email">Date</label>
                         <input type="date" name="date" onChange={(e) => setDate(e.target.value)} id="" />
-                        <button className="button" type="submit">Submit</button>
-                    </form>
-                </div>
+                    </div>
+                    <button className="button formButton" type="submit">Submit</button>
+                </form>
             </div>
+
         </VisitImg>
     );
 }
